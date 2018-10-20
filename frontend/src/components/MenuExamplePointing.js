2e978c91cf803c352644823d12927750ace66cd8
Kuba Cholewa <tom7177@gmail.com>
2018-10-20 21:28:42 +0200

    Menu implemented

---
+++ frontend/src/components/MenuExamplePointing.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Input, Menu, Segment } from 'semantic-ui-react'
+
+export default class MenuExamplePointing extends Component {
+    state = { activeItem: 'map' }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { activeItem } = this.state
+
+        return (
+            <div>
+                <Menu id="menu" pointing>
+                    <img id="logo" src={require('../assets/logos/logo_white.png')}></img>
+                    <Menu.Item id="bigItem" name='map' active={activeItem === 'map'} onClick={this.handleItemClick} />
+                    <Menu.Item id="bigItem"
+                        name='how it works'
+                        active={activeItem === 'how it works'}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item id="bigItem"
+                        name='start earning'
+                        active={activeItem === 'start earning'}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item id="bigItem"
+                        name='contact'
+                        active={activeItem === 'contact'}
+                        onClick={this.handleItemClick}
+                    />
+                </Menu>
+            </div>
+        )
+    }
+}
